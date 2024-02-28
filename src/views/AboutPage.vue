7699dc24ab638d62281c2a0aa6e8afde5abe52ae
PavelVoitov <voitov.pavel@yandex.ru>
2024-02-28 15:55:10 +0300

    fix: layout AboutPage

--- src/views/AboutPage.vue
+++ src/views/AboutPage.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container ml-5 mt-5">
+  <div class="container mx-auto mt-5 px-4">
     <h1 class="text-4xl font-bold mb-4">About Task Manager App</h1>
     <p class="text-lg mb-2">
       Welcome to the Task Manager App, a simple Vue.js application for managing
@@ -9,8 +9,8 @@
       This application allows you to create, edit, and delete tasks easily.
     </span>
 
-    <h2 class="text-2xl font-bold mb-2">Features:</h2>
-    <ul class="list-disc ml-8">
+    <h2 class="text-2xl font-bold mb-2 mx-2">Features:</h2>
+    <ul class="list-disc ml-4 md:ml-8">
       <li>Create new tasks</li>
       <li>Edit existing tasks</li>
       <li>Delete tasks</li>
@@ -18,7 +18,7 @@
     </ul>
 
     <h2 class="text-2xl font-bold mt-8 mb-2">Technologies Used:</h2>
-    <ul class="list-disc ml-8">
+    <ul class="list-disc ml-4 md:ml-8">
       <li>Vue.js</li>
       <li>Vuex for state management</li>
       <li>Vue Router for navigation</li>
